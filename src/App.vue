6586b35c49cfc74f03c5ad2e34a07435791da992
df-hyeongi <hyeongi_kim@daekyo.co.kr>
2025-01-03 14:03:16 +0900

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref, unref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+const text = ref("hello world");
+const textFontSize = ref(1);
+
+// unref 사용
+// https://ko.vuejs.org/api/reactivity-utilities#unref
+console.log("unref", unref(text));
+// value 사용
+console.log("value", text.value);
+
+const create = (text: string) => {
+  console.log("create", text);
+};
+</script>
+
+<!-- APP.vue -->
+<template>
+  <input v-model="text" />
+  <div :style="{ fontSize: textFontSize + 'em' }">
+    <HelloWorld :text="text" @create="create" class="hello" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useAttrs } from "vue";
+
+defineProps<{ text: string }>();
+
+const emits = defineEmits(["create"]);
+
+const handleClick = () => {
+  console.log("clicked");
+  emits("create", "hello");
+};
+</script>
+
+<template>
+  <button @click="handleClick">font 키우기</button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
